9b9f7bb32db4d18879fb29c9c2598acb802b595d
mauricioliveira <cmauricioliveira@gmail.com>
2017-09-06 23:30:11 +0100

    WheaterApp First Commit

---
+++ app/src/main/java/pt/com/mauricioliveira/weatherapp/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.com.mauricioliveira.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
