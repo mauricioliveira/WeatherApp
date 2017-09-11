63b15e0473f034ac5fa50032dc4cfa393b6056b8
mauricioliveira <cmauricioliveira@gmail.com>
2017-09-11 20:04:10 +0100

    WeatherApi

---
+++ app/src/main/java/pt/com/mauricioliveira/weatherapp/Interface/WeatherAPI.java
@@ -0,0 +1,18 @@
+package pt.com.mauricioliveira.weatherapp.Interface;
+
+import pt.com.mauricioliveira.weatherapp.Model.WeatherData;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by moliveira on 11-09-2017.
+ */
+
+public interface WeatherAPI {
+    @GET
+    Call<WeatherData> getCurrentWeather(@Query("lat") double lat,
+                                        @Query("lon") double lon,
+                                        @Query("APPID") String appid,
+                                        @Query("units") String units);
+}
